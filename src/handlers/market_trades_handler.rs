8b43b73a04c7de7b829b657807b60a348ea4b763
pedrosfaria2 <pedroserrano2@gmail.com>
2024-07-08 14:50:29 -0300

    market_trades_handler UPDATE

--- src/handlers/market_trades_handler.rs
+++ src/handlers/market_trades_handler.rs
@@ -4,22 +4,55 @@ use std::time::Duration;
 use crate::api::market_trades::fetch_market_trades;
 use crate::prompt_for_product_id;
 use tokio::time;
+use prettytable::{Table, Row, Cell, format};
+use crate::models::MarketTrade;
 
 pub async fn fetch_market_trades_handler(mut stop: watch::Receiver<()>) -> Result<()> {
     let product_id = prompt_for_product_id();
+    let mut header_printed = false;
     loop {
         tokio::select! {
             _ = stop.changed() => {
                 println!("Stopping fetch_market_trades_handler");
                 break;
             }
-            _ = time::sleep(Duration::from_secs(1)) => {
+            _ = time::sleep(Duration::from_millis(10)) => {
                 match fetch_market_trades(&product_id).await {
-                    Ok(market_trades) => println!("Market Trades for {}: {:?}", product_id, market_trades),
+                    Ok(market_trades) => display_market_trades(&market_trades, &mut header_printed),
                     Err(e) => println!("Error fetching market trades for {}: {:?}", product_id, e),
                 }
             }
         }
     }
     Ok(())
+}
+
+fn display_market_trades(trades: &Vec<MarketTrade>, header_printed: &mut bool) {
+    let mut table = Table::new();
+    table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
+
+    if !*header_printed {
+        table.set_titles(Row::new(vec![
+            Cell::new("Trade ID"),
+            Cell::new("Product ID"),
+            Cell::new("Price"),
+            Cell::new("Size"),
+            Cell::new("Time"),
+            Cell::new("Side"),
+        ]));
+        *header_printed = true;
+    }
+
+    for trade in trades {
+        table.add_row(Row::new(vec![
+            Cell::new(&format!("{:<15}", trade.trade_id)),
+            Cell::new(&format!("{:<10}", trade.product_id)),
+            Cell::new(&format!("{:<10}", trade.price)),
+            Cell::new(&format!("{:<10}", trade.size)),
+            Cell::new(&format!("{:<25}", trade.time)),
+            Cell::new(&format!("{:<5}", trade.side)),
+        ]));
+    }
+
+    table.printstd();
 }
